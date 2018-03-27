6274158134e7c275f748f135e28665d51fa210b6
Kaelig <kaelig.deloumeauprigent@shopify.com>
2018-03-27 11:21:08 -0700

    Try with a wildcard

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.use(morgan("dev"));
 app.locals.MEMCACHE_AGE = MEMCACHE_AGE;
 
 // Automatically redirect to https
-app.set("trust proxy", "loopback");
-app.use((req, res, next) => {
+app.enable("trust proxy");
+app.use("*", (req, res, next) => {
   if (req.secure || process.env.NODE_ENV !== "production") {
     // request was via https or handled locally, so do no special handling
     next();
