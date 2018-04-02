8143abfdac542ff025aa2f0a058d22a8752bc18e
Kaelig <kaelig.deloumeauprigent@shopify.com>
2018-04-01 19:25:47 -0700

    Add a service worker, adding offline support to the top page

--- public/app.js
+++ public/app.js
@@ -123,3 +123,19 @@ document.addEventListener("keydown", handleKeyboardInteractions, false);
 document
   .querySelector(".js-top-stories")
   .addEventListener("keydown", handleStoriesKeyboardInteractions, false);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      (err) => {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
---
+++ public/sw.js
@@ -0,0 +1,55 @@
+const CACHE = "readhn-v1";
+
+// Open a cache and use `addAll()` with an array of assets to add all of them
+// to the cache. Return a promise resolving when all the assets are added.
+const precache = () => caches.open(CACHE).then(cache => cache.addAll(["./"]));
+
+// Time limited network request. If the network fails or the response is not
+// served before timeout, the promise is rejected.
+const fromNetwork = (request, timeout) =>
+  Promise((fulfill, reject) => {
+    // Reject in case of timeout.
+    const timeoutId = setTimeout(reject, timeout);
+    // Fulfill in case of success.
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      // Reject also if network fetch rejects.
+    }, reject);
+  });
+
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
+const fromCache = request =>
+  caches
+    .open(CACHE)
+    .then(cache =>
+      cache
+        .match(request)
+        .then(matching => matching || Promise.reject("no-match"))
+    );
+
+// On install, cache some resource.
+self.addEventListener("install", evt => {
+  console.log("The service worker is being installed.");
+
+  // Ask the service worker to keep installing until the returning promise
+  // resolves.
+  evt.waitUntil(precache());
+});
+
+// On fetch, use cache but update the entry with the latest contents
+// from the server.
+self.addEventListener("fetch", evt => {
+  console.log("The service worker is serving the asset.");
+
+  if (!navigator.onLine) {
+    return evt.respondWith(fromCache(evt.request));
+  }
+
+  // Try network and if it fails, go for the cached copy.
+  evt.respondWith(
+    fromNetwork(evt.request, 2000).catch(() => fromCache(evt.request))
+  );
+});
